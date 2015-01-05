d6d63a56112ae8a5b7af72349d2c1d8e020f77c2
Jenny Kang <jennykang95@gmail.com>
2015-01-05 23:01:49 +0800

    added cookies and started index page

--- login.js
+++ login.js
@@ -2,12 +2,26 @@ $(document).ready(function(){
 	var button = $("#submitButton");
 
 
+	if(docCookies.hasItem("username")){
+		$("#inputUsername").val(docCookies.getItem("username"));
+	}
+
+	if(docCookies.hasItem("password")){
+		$("#inputPassword").val(docCookies.getItem("password"));
+	} 
+
+	if(docCookies.hasItem("enginehost")){
+		$("#inputEngine").val(docCookies.getItem("enginehost"));
+	}
+
 	button.on('click', function(eventOptions){
 		eventOptions.preventDefault(); //prevents the page from refreshing
 
 		var username = $("#inputUsername").val();
 		var password = $("#inputPassword").val();
 		var enginehost = $("#inputEngine").val();
+
+
 
 		if(username.length === 0){
 			$(".alert-danger").removeClass("collapse");
@@ -38,7 +52,15 @@ $(document).ready(function(){
 
 			success: function(data){
 				$(".alert-success").removeClass("collapse");
+				docCookies.setItem("username", username);
+				docCookies.setItem("password", password);
+				docCookies.setItem("enginehost", enginehost);
+
+				// set cookies for username, apiurl and password
+				// then redirect to index.html
+
 				alert(":)");
+				window.location = "index.html";
 			},
 
 			error: function(data){
